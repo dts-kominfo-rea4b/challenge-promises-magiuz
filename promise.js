96ffba27e295ce67435373304a0e01029064de27
aquadevida3 <aquadevida3@gmail.com>
2022-09-19 00:09:33 +0700

    promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (parameter) => {
+  try {
+    const isiTheaterIXX = await promiseTheaterIXX();
+    const isiTheaterVGC = await promiseTheaterVGC();
+    const isi = isiTheaterIXX.concat(isiTheaterVGC);
+    let length = isi.length;
+    let result = 0;
+
+    for (x = 0; x < length; x++) {
+      if (isi[x]["hasil"] == parameter) result++;
+    }
+    return parameter + " = " + result;
+  } catch (pesan) {
+    console.log(pesan);
+  }
+};
 
 module.exports = {
   promiseOutput,
